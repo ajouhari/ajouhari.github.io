bf5bf6818cae6aadbf250ac25cdc7b897c3a714e
Abdulrahman Jouhari <official@ajouhari.com>
2020-07-26 20:53:32 +0300

    changed-site

--- assets/js/map.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-    // The location
-    var location = {lat: 21.4858, lng: 39.1925};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: location});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: location, map: map});
-}
